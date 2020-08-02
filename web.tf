25ea55612cea17e9d2dda87f317022e4600cd1d5
EC2 Default User <ec2-user@ip-10-0-0-47.ca-central-1.compute.internal>
2020-08-02 05:26:25 +0000

    Initial Version

--- web.tf
+++ web.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "WS_SG" {
 //        to_port = 80
 //        protocol = "tcp"
 //        cidr_blocks = ["::/0"]
-    }
+//  }
         ingress {
         from_port = 22
         to_port = 22
