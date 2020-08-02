35e68e7a93c01f6460ab405d3e84b1944e87f095
EC2 Default User <ec2-user@ip-10-0-0-47.ca-central-1.compute.internal>
2020-08-02 05:25:11 +0000

    Initial Version

--- web.tf
+++ web.tf
@@ -11,11 +11,11 @@ resource "aws_security_group" "WS_SG" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-        ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["::/0"]
+//        ingress {
+//        from_port = 80
+//        to_port = 80
+//        protocol = "tcp"
+//        cidr_blocks = ["::/0"]
     }
         ingress {
         from_port = 22
